cc3318a19f17b3d06baf9ccb1390fe1aa9e6a90b
louislu3405 <71736357+louislu3405@users.noreply.github.com>
2021-01-17 19:18:33 -0500

    First version of personal website - Tree Map'

---
+++ geojson.js
@@ -0,0 +1,426 @@
+/*
+var points = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "name": "阿勃勒",
+          "image": "images/GoldenShowerTree.JPG"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            120.67973971366882,
+            24.12386480952657
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "name": "Grinnell Glacier",
+          "image": ""
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            246.27081871032715,
+            48.76037588826086
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "name": "Many Glacier",
+          "image": ""
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            246.34283065795898,
+            48.798284598317906
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#7e7e7e",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "name": "Fossil Beach",
+          "Image": ""
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            283.1444174051285,
+            38.16722890989125
+          ]
+        }
+      }
+    ]
+  }
+*/
+
+var points = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "阿勃勒",
+        "image": "images/GoldenShowerTree.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "蘇木科 Caesalpiniaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67973971366882,
+          24.12386480952657
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "Grinnell Glacier",
+        "image": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          246.27081871032715,
+          48.76037588826086
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "Many Glacier",
+        "image": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          246.34283065795898,
+          48.798284598317906
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "Fossil Beach",
+        "Image": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          283.1444174051285,
+          38.16722890989125
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "蘭嶼烏心石",
+        "image": "images/蘭嶼烏心石.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "木蘭科 Magnoliaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67510485649109,
+          24.12104228551498
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "蒲葵",
+        "image": "images/蒲葵.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "棕梠科 Arecaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67780315876007,
+          24.123500063612834
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "第倫桃",
+        "image": "images/第倫桃.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "第倫桃科 Dilleniaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67794799804686,
+          24.123504959538018
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "猴面果",
+        "image": "images/猴面果.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "桑科 Moraceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67494660615921,
+          24.11954164679192
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "拔蕉",
+        "image": "images/拔蕉.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "芭蕉科 Musaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67515715956688,
+          24.119698321151816
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "藍花楹",
+        "image": "images/藍花楹.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "紫葳科 Bignoniaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67489698529243,
+          24.121490000000000
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "澳洲檫樹",
+        "image": "images/澳洲檫樹.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.6771259009838,
+          24.12207166943813
+        ]
+      }
+    },    
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "墨西哥落羽松",
+        "image": "images/墨西哥落羽松.JPG",
+        "hardsoft": "裸子植物 Gymnosperm",
+        "family": "杉科 Cupressaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67359074950218,
+          24.123279746785375
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "肯氏南洋杉",
+        "image": "images/肯氏南洋杉.JPG",
+        "hardsoft": "裸子植物 Gymnosperm",
+        "family": "南洋杉科 Caesalpiniaceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.6735934317112,
+          24.123354409752736
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "金新木薑子",
+        "image": "images/金新木薑子.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "樟科 Lauraceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67527115345001,
+          24.12163959847886
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "name": "菩提",
+        "image": "images/菩提.JPG",
+        "hardsoft": "被子植物 Angiosperms",
+        "family": "桑科 Moraceae"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          120.67502707242966,
+          24.12149883850283
+        ]
+      }
+    }
+  ]
+}
+
+// add GeoJSON layer to layer
+var myLayer = L.geoJSON(points,{
+  onEachFeature: function (feature,layer) {
+    layer.bindPopup("<div id=\"popupImgBox\"style=\"width: 280px; height: 350px; position: relative;   \">"+ feature.properties.name+ 
+    "<p style=\"margin: 0;\">image:</p>"+
+    "<p style=\"margin: 0;\">"+feature.properties.hardsoft+"</p>"+
+    " <img src=\""+ feature.properties.image+"\" style=\" height: auto; width: 100%; max-height: 300px; max-width: 100% \">"+
+    " </div>").openPopup();
+  }
+}).addTo(mymap);
+//myLayer.addData(points);
+
+function showFloweringPlants() {
+  mymap.removeLayer(myLayer);
+  myLayer = L.geoJSON(points,{
+    onEachFeature: function (feature,layer) {
+      layer.bindPopup("<div id=\"popupImgBox\"style=\"width: 280px; height: 350px; position: relative;   \">"+ feature.properties.name+ 
+      "<p style=\"margin: 0;\">image:</p>"+
+      "<p style=\"margin: 0;\">"+feature.properties.hardsoft+"</p>"+
+      " <img src=\""+ feature.properties.image+"\" style=\" height: auto; width: 100%; max-height: 300px; max-width: 100% \">"+
+      " </div>").openPopup();
+    },
+    filter: function (feature) {
+      return (feature.properties.hardsoft == "被子植物 Angiosperms")
+    }
+  }).addTo(mymap);
+  
+}
+
+
+var plantQuery = []
+
+function query() {
+  mymap.removeLayer(myLayer);
+  myLayer = L.geoJSON(points,{
+    onEachFeature: function (feature,layer) {
+      layer.bindPopup("<div id=\"popupImgBox\"style=\"width: 280px; height: 350px; position: relative;   \">"+ feature.properties.name+ 
+      "<p style=\"margin: 0;\">image:</p>"+
+      "<p style=\"margin: 0;\">"+feature.properties.hardsoft+"</p>"+
+      " <img src=\""+ feature.properties.image+"\" style=\" height: auto; width: 100%; max-height: 300px; max-width: 100% \">"+
+      " </div>").openPopup();
+    },
+    filter: function (feature) {
+      if (plantQuery.length == 0) {
+        return true;
+      }
+      console.log(plantQuery.length);
+      for (i=0;i<plantQuery.length;i++){
+        if (feature.properties.hardsoft == plantQuery[i]) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }).addTo(mymap);
+}
+
+function checkFlower(checkbox, value) {
+  if (checkbox.checked) {
+    plantQuery.push(value);
+  } else {
+    plantQuery.pop(value);
+  }
+  query();
+}
